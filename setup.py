b123279dfddbcae8058783497198c661774c231f
Vladimir Puzakov <vppuzakov@gmail.com>
2019-07-22 23:40:47 +0300

    bump version 0.6.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name='django-taggit-labels',
+    name='rds-django-taggit-labels',
     version=version,
     description="""Alternate labels for django-taggit""",
     long_description=readme + '\n\n' + history,
